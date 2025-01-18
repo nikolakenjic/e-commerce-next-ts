ca401521a8d38ee996d068e57fb1ef48b1610736
Nikola Kenjic <nikola.nico89@gmail.com>
2025-01-18 22:49:01 +0100

    search page added

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchPage = async ({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}) => {
+  const { query } = await searchParams;
+
+  return <div>SearchPage for {query}</div>;
+};
+
+export default SearchPage;
