7bf51f9e2357b4fa3606dec76b332a15959e136e
Nikola Kenjic <nikola.nico89@gmail.com>
2025-01-18 16:54:11 +0100

    shadcn

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div>
       <h1>E-commerce</h1>
+      <Button>Click</Button>
     </div>
   );
 }
