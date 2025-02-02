435348b06f975472a1777d50aeeaa4ed00c2eb3c
Nikola Kenjic <nikola.nico89@gmail.com>
2025-02-02 18:43:41 +0100

    active sale coupon code

---
+++ components/BlackFridayBanner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlackFridayBanner = () => {
+  return <div>BlackFridayBanner</div>;
+};
+
+export default BlackFridayBanner;
---
+++ sanity/lib/sales/couponCode.ts
@@ -0,0 +1,7 @@
+export const COUPON_CODES = {
+  BFRIDAY: 'BRFIDAY',
+  XMAS2024: 'XMAS2024',
+  NYE2025: 'NYE2025',
+};
+
+export type CouponCode = keyof typeof COUPON_CODES;
