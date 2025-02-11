e9e74b3a1900cec4027d371b9c0c126ad7a993f8
Nikola Kenjic <nikola.nico89@gmail.com>
2025-02-11 22:22:40 +0100

    success page

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -37,6 +37,15 @@ export async function createCheckoutSession(
       customerId = customers.data[0].id;
     }
 
+    const baseUrl =
+      process.env.NODE_ENV === 'production'
+        ? `https://${process.env.VERCEL_URL}`
+        : `${process.env.NEXT_PUBIC_BASE_URL}`;
+
+    const successUrl = `${baseUrl}/success?session_id=(CHECKOUT_SESSION_ID)&orderNumber=${metadata.orderNumber}`;
+
+    const cancelUrl = `${baseUrl}/basket`;
+
     // Sessions
     const session = await stripe.checkout.sessions.create({
       customer: customerId,
@@ -45,8 +54,8 @@ export async function createCheckoutSession(
       metadata,
       mode: 'payment',
       allow_promotion_codes: true,
-      success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBIC_BASE_URL}/success?session_id=(CHECKOUT_SESSION_ID)&orderNumber=${metadata.orderNumber}`,
-      cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBIC_BASE_URL}/basket`,
+      success_url: successUrl,
+      cancel_url: cancelUrl,
       line_items: items.map((item) => ({
         price_data: {
           currency: 'usd',
